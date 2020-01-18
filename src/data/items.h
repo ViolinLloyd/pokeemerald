d1f578cbfbd0799d2a96eb45dfb348b797d68577
ViolinLloyd <lloyd99@comcast.net>
2020-01-18 17:06:08 -0500

    Added Trade Stone for trade evolutions

--- src/data/items.h
+++ src/data/items.h
@@ -1312,15 +1312,15 @@ const struct Item gItems[] =
         .secondaryId = 0,
     },
 
-    [ITEM_063] =
+    [ITEM_TRADE_STONE] =
     {
-        .name = _("????????"),
-        .itemId = ITEM_NONE,
+        .name = _("TRADE STONE"),
+        .itemId = ITEM_TRADE_STONE,
         .price = 0,
-        .description = sDummyDesc,
+        .description = sTradeStoneDesc,
         .pocket = POCKET_ITEMS,
-        .type = 4,
-        .fieldUseFunc = ItemUseOutOfBattle_CannotUse,
+        .type = 1,
+        .fieldUseFunc = ItemUseOutOfBattle_EvolutionStone,
         .secondaryId = 0,
     },
 
